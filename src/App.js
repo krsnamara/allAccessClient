68fba429a689107c33b672166df6a75618d43beb
MichaelStarkman <michaelstarkman@gmail.com>
2023-05-20 11:34:59 -0600

    updated import files

--- src/App.js
+++ src/App.js
@@ -2,32 +2,29 @@ import { useState, useEffect } from "react";
 // import { Routes, Route, Navigate } from "react-router-dom";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
-
-
 
 function App() {
   const [user, setUser] = useState(null);
 
-  useEffect (() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => setUser(user))
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => setUser(user));
 
     return () => {
       // cleanup actions
       unsubscribe();
-    }
-
+    };
   }, []);
   return (
     <>
-      <NavBar user={user}/>
-      <Main user={user}/>
+      <NavBar user={user} />
+      <Main user={user} />
       <Footer />
     </>
-  )
-};
+  );
+}
 
 export default App;
