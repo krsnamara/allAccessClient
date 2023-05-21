70c84ae1c8964fd4ebdaa696d0c034ba7620242f
nangel42 <az_nick_angel@ymail.com>
2023-05-20 18:04:09 -0700

    Map Component added for Homepage

---
+++ src/components/HomeMapView/HomeMapView.jsx
@@ -0,0 +1,18 @@
+import './HomeMapView.css'
+
+
+export default function HomeMapView() {
+
+    return (
+        <>
+            <h1>Map</h1>
+
+            <div className="map-container">
+                <div className="map">
+                    <img src="https://developers.google.com/static/maps/images/landing/hero_maps_static_api.png" className="map-img" alt="..." />
+                </div>
+            </div>
+
+        </>
+    );  
+}
---
+++ src/components/MapToggle/MapToggle.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MapToggle.css';
+
+export default function MapToggle({ onToggle }) {
+  const handleToggle = () => {
+    onToggle();
+  };
+
+  return (
+    <div className="checkbox-wrapper-8">
+      <input type="checkbox" id="cb3-8" className="tgl tgl-skewed" onChange={handleToggle} />
+      <label htmlFor="cb3-8" data-tg-on="Map" data-tg-off="Grid" className="tgl-btn"></label>
+    </div>
+  );
+}
