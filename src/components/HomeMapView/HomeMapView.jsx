2c6a99485e4c0323d7128034edcc43feb3b1e91a
nangel42 <az_nick_angel@ymail.com>
2023-05-21 12:56:11 -0700

    Main Page Map Done

--- src/components/HomeMapView/HomeMapView.jsx
+++ src/components/HomeMapView/HomeMapView.jsx
@@ -7,82 +7,85 @@ export default function HomeMapView() {
     <>
       <h1>Map</h1>
 
-      <div className="container">
-        <div className="map-container">
-          <div className="map">
-            <img
-              src="https://developers.google.com/static/maps/images/landing/hero_maps_static_api.png"
-              className="map-img"
-              alt="..."
-            />
-          </div>
-        </div>
-
-        <div className="card-container">
-          <div className="card">
-            <div className="card-picture">
+      <div className="home-map-container">
+        <div className="home-card-container">
+          <div className="home-card">
+            <div className="home-card-picture">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
-                className="card-img-top"
+                className="home-card-img-top"
                 alt="..."
               />
             </div>
-            <div className="card-body">
-              <h5 className="card-title">Cooking Class</h5>
-              <p className="card-text">This is a cooking class</p>
-              <div className="location">
+            <div className="home-card-body">
+              <h5 className="home-card-title">Cooking Class</h5>
+              <p className="home-card-text">This is a cooking class</p>
+              <div className="home-location">
                 <FaMapMarkerAlt size={24} color="green" />
-                <p className="location-text">Oakland, CA</p>
+                <p className="home-location-text">Oakland, CA</p>
               </div>
-              <a href="#" className="btn btn-primary">
+              <a href="#" className="home-btn btn-primary">
                 link
               </a>
             </div>
           </div>
 
-          <div className="card">
-            <div className="card-picture">
+          <div className="home-card">
+            <div className="home-card-picture">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
-                className="card-img-top"
+                className="home-card-img-top"
                 alt="..."
               />
             </div>
-            <div className="card-body">
-              <h5 className="card-title">Cooking Class</h5>
-              <p className="card-text">This is a cooking class</p>
-              <div className="location">
+            <div className="home-card-body">
+              <h5 className="home-card-title">Cooking Class</h5>
+              <p className="home-card-text">This is a cooking class</p>
+              <div className="home-location">
                 <FaMapMarkerAlt size={24} color="green" />
-                <p className="location-text">Oakland, CA</p>
+                <p className="home-location-text">Oakland, CA</p>
               </div>
-              <a href="#" className="btn btn-primary">
+              <a href="#" className="home-btn btn-primary">
                 link
               </a>
             </div>
           </div>
 
-          <div className="card">
-            <div className="card-picture">
+          <div className="home-card">
+            <div className="home-card-picture">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
-                className="card-img-top"
+                className="home-card-img-top"
                 alt="..."
               />
             </div>
-            <div className="card-body">
-              <h5 className="card-title">Cooking Class</h5>
-              <p className="card-text">This is a cooking class</p>
-              <div className="location">
+            <div className="home-card-body">
+              <h5 className="home-card-title">Cooking Class</h5>
+              <p className="home-card-text">This is a cooking class</p>
+              <div className="home-location">
                 <FaMapMarkerAlt size={24} color="green" />
-                <p className="location-text">Oakland, CA</p>
+                <p className="home-location-text">Oakland, CA</p>
               </div>
-              <a href="#" className="btn btn-primary">
+              <a href="#" className="home-btn btn-primary">
                 link
               </a>
             </div>
           </div>
         </div>
+
+        <div className="home-map">
+          <img
+            src="https://developers.google.com/static/maps/images/landing/hero_maps_static_api.png"
+            className="home-map-img"
+            alt="..."
+          />
+        </div>
+
+        
       </div>
     </>
   );
 }
+
+
+
