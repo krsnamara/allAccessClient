436d02b4a37e740cfdab77d58d37aaf70d61db20
krsnamara <martinj.fitzpatrick@gmail.com>
2023-05-22 11:49:12 -0400

    nav modal WIP only thing left is some slight justify and align, sign up white background fill and links

--- src/components/NavLoginModal/NavLoginModal.js
+++ src/components/NavLoginModal/NavLoginModal.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { logout, login } from "../../firebase";
 import LoginHamburger from '../../assets/buttons-icons/login_hamburger.png';
+import LoginIcon from '../../assets/buttons-icons/loginModalicon.png';
+import ProfileSmallCircle from '../../assets/buttons-icons/profileSmallCircle.png';
+import CreateEvent from '../../assets/buttons-icons/mdi_events.png';
+import SettingsIcon from '../../assets/buttons-icons/settings.png';
+import LoginLogoutIcon from '../../assets/buttons-icons/loginLogOutIcon.png';
 import "./NavLoginModal.css";
 
 export default function Modal({ user }) {
@@ -28,17 +33,42 @@ export default function Modal({ user }) {
         <div className="modal"> 
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>Hello Modal</h2>
+            <div className="Xicon">
+                <p className="closingX" onClick={toggleModal} >X</p>
+                <img src={LoginIcon} alt="loginIcon" className="loginIcon" />
+            </div>
             <div className="navBarModalWrapper">
               {user ? (
-                <button onClick={logout}>Log Out</button>
+                <div className="loginLogoutWrapper">
+                <div className="profileCircleWrapper">
+                    <img src={ProfileSmallCircle} alt="profileSmallCircle" className="profileSmallCircle" />
+                    <p className="loginLogout" onClick={login} >Profile</p>
+                </div>
+                <div className="createEventWrapper">
+                    <img src={CreateEvent} alt="createEvent" className="createEvent" />
+                    <p className="loginLogout" onClick={login} >Create Event</p>    
+                </div>
+                <div className="settingsWrapper">
+                <img src={SettingsIcon} alt="settings" className="settings" />
+                <p className="loginLogout" onClick={login} >Settings</p>
+                </div>
+                <div className="loginWrapper">
+                    <img src={LoginLogoutIcon} alt="loginLogoutIcon" className="loginLogoutIcon" />
+                    <p className="loginLogout" onClick={login} >Log out</p>
+                </div>
+                </div>
               ) : (
-                <button onClick={login}>Log In</button>
+                <div className="loginLogoutWrapper">
+                    <div className="loginWrapper">
+                        <img src={LoginLogoutIcon} alt="loginLogoutIcon" className="loginLogoutIcon" />
+                        <p className="loginLogout" onClick={login} >Login</p>
+                    </div>
+                    <div className="loginWrapper">
+                        <p className="loginLogout" >Sign Up</p>
+                    </div>
+                </div>
               )}
             </div>
-            <button className="close-modal" onClick={toggleModal}>
-              CLOSE
-            </button>
           </div>
         </div>
       )}
