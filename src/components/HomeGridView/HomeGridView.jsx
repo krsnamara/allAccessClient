69fce5df161fee5866439a3a454b4ed4804ef0b9
nangel42 <az_nick_angel@ymail.com>
2023-05-21 17:42:22 -0700

    Pictures added some css changed

--- src/components/HomeGridView/HomeGridView.jsx
+++ src/components/HomeGridView/HomeGridView.jsx
@@ -1,124 +1,197 @@
 import './HomeGridView.css'
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import LikeButton from '../../components/LikeButton/LikeButton';
+import CookingClass from '../../assets/Cooking-Class.png';
+import DanceClass from '../../assets/Dance-Class.png';
+import DJDanceParty from '../../assets/DJ-Dance-Party.png';
+import GhostTours from '../../assets/Ghost-Tours.png';
+import GroupHiking from '../../assets/Group-Hiking.png';
+import HistoricTour from '../../assets/Historic-Tour.png';
+import WineTasting from '../../assets/Wine-Tasting.png';
+import ArcadePlayDay from '../../assets/Arcade-Play-Day.png';
+
 
 export default function HomeGridView() {
 
     return (
         <>
             <h1>
-                Popular
+                Upcoming
             </h1>
 
 
             <div className="container">
                 <div className="card-container">
+
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={CookingClass} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Cooking Class</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.0</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
-                    </div>
                     </div>
 
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={ArcadePlayDay} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Arcade Play Day</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
-                    </div>
                     </div>
 
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={HistoricTour} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Historic Tour</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
-                    </div>
                     </div>
 
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={WineTasting} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Wine Tasting</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
                     </div>
+                
+                </div>
+            </div>
+
+            <h1>
+                Highest Ratings
+            </h1>
+
+            <div className="container">
+                <div className="card-container">
+                    
+
+                    <div className="card">
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={DanceClass} className="card-img-top" alt="..." />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Dance Class</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={GhostTours} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">Ghost Tours</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
-                    </div>
                     </div>
 
                     <div className="card">
-                    <div className="card-picture">
-                        <div className="like-button">
-                        <LikeButton />
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={DJDanceParty} className="card-img-top" alt="..." />
                         </div>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930" className="card-img-top" alt="..." />
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Cooking Class</h5>
-                        <p className="card-text">This is a cooking class</p>
-                        <div className="location">
-                        <FaMapMarkerAlt size={24} color="green" />
-                        <p className="location-text">Oakland, CA</p>
+                        <div className="card-body">
+                            <h5 className="card-title">DJ Dance Party</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
                         </div>
-                        <a href="#" className="btn btn-primary">link</a>
                     </div>
+
+                    <div className="card">
+                        <div className="card-picture">
+                            <div className="like-button">
+                            <LikeButton />
+                            </div>
+                            <img src={GroupHiking} className="card-img-top" alt="..." />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">Group Hiking</h5>
+                            <div className="location">
+                                <FaMapMarkerAlt size={24} color="green" />
+                                <p className="location-text">Oakland, CA</p>
+                            </div>
+                            <div className="Rating">
+                                <p className="rating-text">Rating: 4.5</p>
+                                <p className="rating-amount">{"(487)"}</p>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
