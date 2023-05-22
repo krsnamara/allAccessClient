c7fb23cf1ece625a5a9f406c28dff6bb693031ed
krsnamara <martinj.fitzpatrick@gmail.com>
2023-05-22 18:38:10 -0400

    events index

--- src/pages/Evnts/EvntsIndex/EvntsIndex.jsx
+++ src/pages/Evnts/EvntsIndex/EvntsIndex.jsx
@@ -1,33 +1,43 @@
 import { Link } from "react-router-dom";
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import LikeButton from '../../../components/LikeButton/LikeButton';
 import './EvntsIndex.css';
 
-
 function EvntsIndex(props) {
+  // loaded function
+  const loaded = () => {
+    return props.evnts.map((evnt) => (
+        <div key={evnt._id} className="evntsIndexWrapper">
+            <Link to={`/evnts/${evnt._id}`}>
+            <div className="evntcard">
+                            <div className="evntcard-picture">
+                                <div className="like-button">
+                                <LikeButton />
+                                </div>
+                                <img src={evnt.image} className="evntcard-img-top" alt={evnt.name} />
+                            </div>
+                            <div className="evntcard-body">
+                                <h5 className="evntcard-title">{evnt.eventType}</h5>
+                                <div className="location">
+                                    <FaMapMarkerAlt size={24} color="green" />
+                                    <p className="location-text">{evnt.address}</p>
+                                </div>
+                                <div className="Rating">
+                                    <p className="rating-text">Rating: 4.0</p>
+                                    <p className="rating-amount">{"(487)"}</p>
+                                </div>
+                            </div>
+                        </div>
+            </Link>
+        </div>
+    ));
+  };
 
-    //loaded function
-    const loaded = () => {
-        return props.evnts.map(evnt => (
-            <div className="evntsIndexWrapper">
-                <div key={evnt._id} className="evntsCard">
-                    <Link to={`/evnts/${evnt._id}`}>
-                        <h1>{evnt.name}</h1>
-                    </Link>
-                    <img src={evnt.image} alt={evnt.name} className="evntsImage" />
-                    <h3>{evnt.eventType}</h3>
-                </div>
-            </div>
-        ));
-    };
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
 
-    const loading = () => {
-        return <h1>Loading...</h1>
-    };
-
-    return (
-        <section>
-            { props.evnts ? loaded() : loading() }
-        </section>
-    )
-  }
+  return <section className="flex-container">{props.evnts ? loaded() : loading()}</section>;
+}
 
 export default EvntsIndex;
