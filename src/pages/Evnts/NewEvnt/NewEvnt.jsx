d4468367de1b9037085038f58e93f1cf80856e15
MichaelStarkman <michaelstarkman@gmail.com>
2023-05-22 13:45:11 -0600

    newCreateEvents

--- src/pages/Evnts/NewEvnt/NewEvnt.jsx
+++ src/pages/Evnts/NewEvnt/NewEvnt.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import './NewEvnt.css'
 
+import { Link, useNavigate } from "react-router-dom";
+import "./NewEvnt.css";
+import { IoChevronBackCircleOutline } from "react-icons/io5";
 
 function NewEvnt(props) {
   const formFields = {
     name: "",
     image: "",
-    title: ""
+    title: "",
   };
 
   const [newForm, setNewForm] = useState(formFields);
@@ -16,7 +17,7 @@ function NewEvnt(props) {
   const handleChange = (event) => {
     setNewForm({
       ...newForm,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
@@ -29,51 +30,104 @@ function NewEvnt(props) {
 
   return (
     <>
-      <section>
-        <form onSubmit={handleSubmit}>
+      <section className="new-event-form-container">
+        <div className="back-button-container">
+          <Link className="back-button-link">
+            <IoChevronBackCircleOutline className="back-link" />
+            Back
+          </Link>
+        </div>
+        <h2 className="create-new-event-title">Create New Event</h2>
+        <form onSubmit={handleSubmit} className="new-event-form">
+          <h4 className="input-title">Event Name*</h4>
           <input
+            className="create-new-event-input"
             type="text"
             name="name"
             onChange={handleChange}
             value={newForm.name}
-            placeholder="Whats the Name?"
+            placeholder="Enter event name"
+            required
           />
+          <h4 className="input-title">Event Type*</h4>
           <input
+            className="create-new-event-input"
             type="text"
-            name="eventType"
+            name="name"
             onChange={handleChange}
             value={newForm.eventType}
-            placeholder="What is the eventType"
+            placeholder="Enter event type"
+            required
           />
+          {/* TODO: Change to Drop Down*/}
+          {/* <select
+            className="create-new-event-input"
+            id="event-types" 
+            name="event-types"
+          >
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
           <input
+            type="submit"
+            name="eventType"
+            onSubmit={handleChange}
+            value={newForm.eventType}
+            placeholder="Enter event type"
+            required */}
+          <h4 className="input-title">Add photo URL</h4>
+          <input
+            className="create-new-event-input"
             type="text"
             name="image"
             onChange={handleChange}
             value={newForm.image}
             placeholder="Enter URL of Image"
           />
-          <input
+          <h4 className="input-title">Event Description*</h4>
+          <textarea
+            className="create-new-event-textarea"
             type="text"
-            name="description"
+            name="name"
             onChange={handleChange}
             value={newForm.description}
-            placeholder="What is the description"
+            placeholder="A few details about your event"
+            required
+            rows="8"
+            cols="30"
+            align="middle"
           />
+          {/* TODO: change to drop down */}
+          <h4 className="input-title">Reservation Required*</h4>
           <input
+            className="create-new-event-input"
             type="text"
             name="reservation"
             onChange={handleChange}
             value={newForm.reservation}
-            placeholder="What is the reservation"
+            placeholder="Are reservations required?"
+            required
           />
+          <h4 className="input-title">Event Website*</h4>
           <input
+            className="create-new-event-input"
             type="text"
             name="website"
             onChange={handleChange}
             value={newForm.website}
-            placeholder="What is the website"
+            placeholder="Please enter http://format"
+            required
           />
           <input
+            className="input-submit-button"
+            type="submit"
+            value="Create Event"
+          />
+
+          {/* STREtCH GOALS */}
+          {/* <input
             type="text"
             name="suitability"
             onChange={handleChange}
@@ -108,7 +162,7 @@ function NewEvnt(props) {
             value={newForm.atractions}
             placeholder="Attractions Here"
           />
-          <input type="submit" value="Create Event" />
+          <input type="submit" value="Create Event" /> */}
         </form>
       </section>
     </>
