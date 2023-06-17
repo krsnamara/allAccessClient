007081efb4f51aeb6ef8fceef5d08ee092b45c9b
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-16 22:34:03 -0400

    like button styling and dyno card styling

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import LocationIcon from "../../assets/buttons-icons/location.svg";
 import LikeButton from "../LikeButton/LikeButton";
 import "./ExperienceCard.css";
 
 export default function SinglePost({ image }) {
   return (
-    <div className="card">
-      <div className="card-picture">
+    <div className="expCard">
+      <div className="expCard-heart">
         <div className="like-button">
           <LikeButton />
         </div>
         <img
           src={image.imageUrl}
-          className="card-img-top"
+          className="expCard-img"
           alt={image.imageName}
         />
       </div>
@@ -20,14 +20,18 @@ export default function SinglePost({ image }) {
         to={`/images/${image._id}`}
         style={{ color: "black", textDecoration: "none" }}
       >
-        <div className="card-body">
-          <h5 className="card-title">{image.name}</h5>
-          <div className="location">
-            <FaMapMarkerAlt size={24} color="green" />
-            <p className="location-text">{image.address}</p>
+        <div className="expCard-body">
+          <p className="expCard-title">{image.name}</p>
+          <div className="expLocation-wrapper">
+            <img
+              src={LocationIcon}
+              alt="locationIcon"
+              className="expLocationIcon"
+            />
+            <p className="expLocationText">{image.address}</p>
           </div>
-          <div className="Rating">
-            <p className="rating-text">Rating: 4.0</p>
+          <div className="ratingWrapper">
+            <p className="ratingText">4.0</p>
             <p className="rating-amount">{"(487)"}</p>
           </div>
         </div>
