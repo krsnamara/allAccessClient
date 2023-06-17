2d1424061e395ed0e9053b45c1d9553aaf10daac
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-17 01:30:52 -0400

    google map styling

---
+++ src/components/MapView/MapView.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import MapAPI from "../MapAPI/MapAPI";
+import "./MapView.css";
+
+export default function MapView() {
+  return (
+    <>
+      <div className="home-map-container">
+        <div className="home-card-container">
+          <div className="home-card">
+            <div className="home-card-picture">
+              <img
+                src="https://i.imgur.com/dX7f3DJ.jpg"
+                className="home-card-img-top"
+                alt="cardImage"
+              />
+            </div>
+            <div className="home-card-body">
+              <h5 className="home-card-title">Meet up at Cheeseman Park</h5>
+              <p className="home-card-text">Event Details. Go here.</p>
+              <div className="home-location">
+                <FaMapMarkerAlt size={24} color="green" />
+                <p className="home-location-text">Denver, CO</p>
+              </div>
+              {/* eslint-disable-next-line */}
+              <a href="#" className="home-btn btn-primary">
+                link
+              </a>
+            </div>
+          </div>
+
+          <div className="home-card">
+            <div className="home-card-picture">
+              <img
+                src="https://i.imgur.com/GpVxLfW.jpg"
+                className="home-card-img-top"
+                alt="..."
+              />
+            </div>
+            <div className="home-card-body">
+              <h5 className="home-card-title">Cooking Class</h5>
+              <p className="home-card-text">This is a cooking class</p>
+              <div className="home-location">
+                <FaMapMarkerAlt size={24} color="green" />
+                <p className="home-location-text">Oakland, CA</p>
+              </div>
+              {/* eslint-disable-next-line */}
+              <a href="#" className="home-btn btn-primary">
+                link
+              </a>
+            </div>
+          </div>
+
+          <div className="home-card">
+            <div className="home-card-picture">
+              <img
+                src="https://i.imgur.com/M3famJE.jpg"
+                className="home-card-img-top"
+                alt="..."
+              />
+            </div>
+            <div className="home-card-body">
+              <h5 className="home-card-title">
+                Hybrid: Friday Disability Peer Support Group
+              </h5>
+              <p className="home-card-text">
+                SIGN UP TO JOIN THE GROUP! This hybrid peer support group is for
+                individuals who have a disability and identify as a member of
+                the LGBTQ+ community.
+              </p>
+              <div className="home-location">
+                <FaMapMarkerAlt size={24} color="green" />
+                <p className="home-location-text">E</p>
+              </div>
+              {/* eslint-disable-next-line */}
+              <a href="#" className="home-btn btn-primary">
+                link
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-map">
+          <MapAPI />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import MapToggle from '../../components/MapToggle/MapToggle';
-import HomeGridView from '../../components/HomeGridView/HomeGridView';
-import HomeMapView from '../../components/HomeMapView/HomeMapView';
-import Filter from '../../assets/Navigation_Filters.png';
-import './Home.css';
+import React, { useState } from "react";
+import MapToggle from "../../components/MapToggle/MapToggle";
+import DynamicHome from "../../components/DynamicHome/DynamicHome";
+import MapView from "../../components/MapView/MapView";
+import Filter from "../../assets/Navigation_Filters.png";
+import "./Home.css";
 
-function Home() {
+function Home(props) {
   const [showMap, setShowMap] = useState(false);
 
   const handleToggleMap = () => {
@@ -16,21 +16,21 @@ function Home() {
     <>
       <div className="buttons">
         <MapToggle onToggle={handleToggleMap} />
-        <button><img src={Filter} alt="Filter" className="filter-image" /></button>
+        <button>
+          <img src={Filter} alt="Filter" className="filter-image" />
+        </button>
         <button>All</button>
         <button className="mobile-hide">Classes & Workshops</button>
         <button className="mobile-hide">Bars</button>
         <button className="mobile-hide">Category</button>
       </div>
 
-
-
       {showMap ? (
         // Render map component
-        <HomeMapView />
+        <MapView />
       ) : (
         // Render grid component
-        <HomeGridView />
+        <DynamicHome images={props.images} />
       )}
     </>
   );
